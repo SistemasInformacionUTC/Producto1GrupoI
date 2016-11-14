2e0d47de017cfd2bfcfed368ea0d5c1703829c99
baguaizaUTC <blankisbz1903@gmail.com>
2016-11-14 09:54:07 -0500

    codigo

--- play-ebean-example-master/app/models/Disco.java
+++ play-ebean-example-master/app/models/Disco.java
@@ -1,11 +1,27 @@
 package models;
 
-public class Disco {
-public Long id;
-	public String nombre;
-	public String marca;
-	public int precio;
-public void setnombre(){
-	return nombre;
-}	
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.Constraint;
+
+import com.avaje.ebean.Model;
+
+public class Disco  extends Model {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    
+	public Long id;
+    
+    @Constraint.Required(message=" ingresar nombre del disco.")
+    public String nombre;
+    
+    @Constraints.Required(message=" ingresar su marca.")
+    public String descripcion;
+    
+    @Constraints.Required(message="");
+	
 }
+
